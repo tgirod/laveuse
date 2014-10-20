9efa7e3d6068ec805389935d612e607823f23ffc
Thomas Girod <girodt@gmail.com>
2014-10-20 16:12:51 +0200

    améliorations diverses

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 ARDUINO_DIR = /usr/share/arduino
 USER_LIB_PATH = ../libraries
-ARDUINO_LIBS = 
+ARDUINO_LIBS = SPI Bounce2
 AVR_TOOLS_DIR = /usr
-ARDUINO_PORT = /dev/ttyACM0
-BOARD_TAG = uno
+ARDUINO_PORT = /dev/ttyUSB0
+BOARD_TAG = mega328
 include /usr/share/arduino/Arduino.mk
--- laveuse.ino
+++ laveuse.ino
@@ -1,21 +1,23 @@
+#include <Bounce2.h>
 #include <SPI.h>
-// include Playing With Fusion MAX31865 libraries
-#include <PlayingWithFusion_MAX31865.h>              // core library
-#include <PlayingWithFusion_MAX31865_STRUCT.h>       // struct library
+#include <setjmp.h>
+
+#include <PlayingWithFusion_MAX31865.h>        // core library
+#include <PlayingWithFusion_MAX31865_STRUCT.h> // struct library
 
 /* interface utilisateur */
-#define PIN_ARRET 2        // arrêt d'urgence (int.0)
-#define PIN_NETTOYAGE 3    // lancement du nettoyage
-#define PIN_DESINFECTION 4 // lancement de la désinfection
-#define PIN_BEEP 5         // avertisseur sonore
+#define PIN_ARRET 2         // arrêt d'urgence (fil jaune) -- interruption
+#define PIN_NETTOYAGE A2    // lancement du nettoyage (fil rouge)
+#define PIN_DESINFECTION A1 // lancement de la désinfection (fil orange)
+#define PIN_BEEP A3         // avertisseur sonore
 
 /* asservissement */
-#define PIN_RESEAU A0  // prise d'eau du réseau
-#define PIN_EGOUT A1   // écoulement vers l'égout
-#define PIN_BAC A2     // entrée circuit fermé
-#define PIN_RECUP A3   // écoulement circuit fermé
-#define PIN_POMPE A4   // pompe électrique
-#define PIN_CHAUFFE A5 // thermoplongeur
+#define PIN_RESEAU A0 // prise d'eau du réseau (fil blanc)
+#define PIN_EGOUT 3   // écoulement vers l'égout (fil gris)
+#define PIN_BAC 4     // entrée circuit fermé (fil violet)
+#define PIN_RECUP 5   // écoulement circuit fermé (fil bleu)
+#define PIN_POMPE 6   // pompe électrique (fil orange)
+#define PIN_CHAUFFE 7 // thermoplongeur (fil rouge)
 
 /* capteur */
 #define PIN_SONDE 10 // CS PIN pour la sonde
@@ -24,20 +26,21 @@
 // SCK 13
 
 /* constantes diverses */
-#define TEMP_MIN 70           // on ne nettoie pas en dessous de cette température
-#define TEMP_MAX 80           // couper la chauffe au dessus de cette température
+#define TEMP_MIN 27           // on ne nettoie pas en dessous de cette température
+#define TEMP_MAX 30           // couper la chauffe au dessus de cette température
 #define CYCLES_LAVAGE 3       // recommencer X fois
 #define DUREE_LAVAGE 60       // balancer la soude X secondes
 #define DUREE_DESINFECTION 60 // balancer le désinfectant X secondes
 #define DUREE_ECOULEMENT 10   // temps laissé pour l'écoulement de l'eau
 
 /* quelques macros pour rendre le code plus explicite */
-#define reseau(X) digitalWrite(PIN_RESEAU, X)
-#define egout(X) digitalWrite(PIN_EGOUT, X)
-#define bac(X) digitalWrite(PIN_BAC, X)
-#define recup(X) digitalWrite(PIN_RECUP, X)
-#define pompe(X) digitalWrite(PIN_POMPE, X)
-#define chauffe(X) digitalWrite(PIN_CHAUFFE, X)
+#define reseau(X) digitalWrite(PIN_RESEAU, !X)
+#define egout(X) digitalWrite(PIN_EGOUT, !X)
+#define bac(X) digitalWrite(PIN_BAC, !X)
+#define recup(X) digitalWrite(PIN_RECUP, !X)
+#define pompe(X) digitalWrite(PIN_POMPE, !X)
+#define chauffe(X) digitalWrite(PIN_CHAUFFE, !X)
+#define chauffeRead !digitalRead(PIN_CHAUFFE)
 
 //Sensor addresses:
 const byte CONFIG_REG_W = 0x80; // Config register write addr
@@ -47,6 +50,11 @@ const byte ADC_FAULT_REG = 0x07; // Addr of the fault reg
 
 PWFusion_MAX31865_RTD sonde(PIN_SONDE);
 
+// Boutons pour lancer les cycles de nettoyage et désinfection
+Bounce btnNettoyage = Bounce();
+Bounce btnDesinfection = Bounce();
+
+jmp_buf env; // pour faire le saut en cas d'arrêt d'urgence
 
 /* l'état dans lequel se trouve la laveuse */
 static enum {ARRET, NETTOYAGE, DESINFECTION} etat;
@@ -62,21 +70,18 @@ double temperature() {
 
     sonde.MAX31865_full_read(&sonde_reg);          // Update MAX31855 readings 
 
-    // Print information to serial port
-    Serial.println("RTD Sensor 0:");              // Print RTD0 header
-
     if(0 == sonde_reg.status)                       // no fault, print info to serial port
     {
         // calculate RTD temperature (simple calc, +/- 2 deg C from -100C to 100C)
         // more accurate curve can be used outside that range
         tmp = ((double)sonde_reg.rtd_res_raw / 32) - 256;
-        Serial.print(tmp);                          // print RTD resistance
-        Serial.println(" deg C");                   // print RTD temperature heading
+        //Serial.print(tmp);                          // print RTD resistance
+        //Serial.println(" deg C");                   // print RTD temperature heading
     }  // end of no-fault handling
     else 
     {
         Serial.print("RTD Fault, register: ");
-        Serial.print(sonde_reg.status);
+        Serial.println(sonde_reg.status);
         if(0x80 & sonde_reg.status)
         {
             Serial.println("RTD High Threshold Met");  // RTD high threshold fault
@@ -111,31 +116,46 @@ double temperature() {
 }
 
 /*
+ * arrêt d'urgence. On coupe tout, on ferme tout.
+ */
+void arreter() {
+    if (etat != ARRET) {
+        etat = ARRET;
+        Serial.println("Arret d'urgence");
+        pompe(0);
+        chauffe(0);
+        reseau(0);
+        bac(0);
+        delay(DUREE_ECOULEMENT*1000);
+        recup(0);
+        egout(0);
+    }
+}
+
+/*
  * attendre X secondes, vérifier la chauffe toutes les secondes et lancer la
  * procédure d'arrêt d'urgence si nécessaire.
  */
-int attendre(unsigned int secondes) { 
+void attendre(unsigned int secondes) { 
     unsigned long t = millis();
     while(millis()-t < secondes*1000UL){
-        // arrêt d'urgence
+        // arrêt d'urgence : longjmp pour sortir de la fonction
         if (etat == ARRET) {
-            Serial.println("Arret d'urgence");
-            return 1;
+            longjmp(env, 1);
         }
         // contrôle de la chauffe
         if (etat == NETTOYAGE) {
-            if (temperature() < TEMP_MIN) { 
+            if (temperature() < TEMP_MIN && !chauffeRead) { 
                 Serial.println("Activer la chauffe");
                 chauffe(1);
             }
-            if (temperature() > TEMP_MAX) {
+            if (temperature() > TEMP_MAX && chauffeRead) {
                 Serial.println("Couper la chauffe");
                 chauffe(0);
             }
         }
         delay(1000); 
     }
-    return 0;
 }
 
 /* cycle de rincage */
@@ -146,9 +166,9 @@ void rincage(int secondes) {
     recup(0);
     egout(1);
     reseau(1);
-    attendre(secondes*1000);
+    attendre(secondes);
     reseau(0);
-    if (attendre(DUREE_ECOULEMENT)) return;
+    attendre(DUREE_ECOULEMENT);
     egout(0);
 }
 
@@ -166,16 +186,16 @@ void nettoyage() {
     rincage(10); 
     // attente de la chauffe
     while (temperature() < TEMP_MIN) {
-        if (attendre(1)) return;
+        attendre(10);
     }
     recup(1);
     bac(1);
     // nettoyage
     for (int i=0; i<CYCLES_LAVAGE; i++) {
         pompe(1);
-        if (attendre(DUREE_LAVAGE)) return;
+        attendre(DUREE_LAVAGE);
         pompe(0);
-        if (attendre(DUREE_ECOULEMENT)) return;
+        attendre(DUREE_ECOULEMENT);
     }
     bac(0);
     recup(0);
@@ -192,9 +212,9 @@ void desinfection() {
     recup(1);
     bac(1);
     pompe(1);
-    if (attendre(DUREE_DESINFECTION)) return;
+    attendre(DUREE_DESINFECTION);
     pompe(0);
-    if (attendre(DUREE_ECOULEMENT)) return;
+    attendre(DUREE_ECOULEMENT);
     bac(0);
     recup(0);
     rincage(10);
@@ -202,25 +222,15 @@ void desinfection() {
     beep();
 }
 
-/*
- * arrêt d'urgence. On coupe tout, on ferme tout.
- */
-void arreter() {
-    pompe(0);
-    chauffe(0);
-    delay(DUREE_ECOULEMENT);
-    reseau(0);
-    bac(0);
-    recup(0);
-    egout(0);
-    etat = ARRET;
-}
-
 void setup() {
     // interface utilisateur
     pinMode(PIN_ARRET, INPUT_PULLUP);
     pinMode(PIN_NETTOYAGE, INPUT_PULLUP);
+    btnNettoyage.attach(PIN_NETTOYAGE);
+    btnNettoyage.interval(5);
     pinMode(PIN_DESINFECTION, INPUT_PULLUP);
+    btnDesinfection.attach(PIN_DESINFECTION);
+    btnDesinfection.interval(5);
     pinMode(PIN_BEEP, OUTPUT);
     // relais (4 électrovannes + chauffe + sonde (SPI CS)
     pinMode(PIN_RESEAU, OUTPUT);
@@ -242,6 +252,7 @@ void setup() {
     attachInterrupt(0, arreter, FALLING);
     // initialiser la lecture de température
     Serial.begin(9600);
+    Serial.println("serial ok");
     SPI.begin();
     SPI.setDataMode(SPI_MODE3);
     sonde.MAX31865_config();
@@ -249,13 +260,17 @@ void setup() {
 }
 
 void loop() {
-    // démarrage du nettoyage
-   if(digitalRead(PIN_NETTOYAGE)==LOW) {
-       nettoyage();
-   }
-   // démarrage de la désinfection
-   if(digitalRead(PIN_DESINFECTION)==LOW) {
-       desinfection();
-   }
+    setjmp(env);
+    Serial.println("en attente d'une commande");
+    while (1) {
+        // démarrage du nettoyage
+        if(btnNettoyage.update() && btnNettoyage.read() == LOW) {
+            nettoyage();
+        }
+        // démarrage de la désinfection
+        if(btnDesinfection.update() && btnDesinfection.read() == LOW) {
+            desinfection();
+        }
+    }
 }
 
