dea55a4bc7cf31ea360287c05c38bf3f295fdde7
Tomtom <tomtom@herbesfolles.org>
2015-01-28 10:24:59 +0100

    retirer la gestion de la temp

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 ARDUINO_DIR = /usr/share/arduino
 USER_LIB_PATH = ../libraries
-ARDUINO_LIBS = SPI Bounce2
+ARDUINO_LIBS = Bounce2
 AVR_TOOLS_DIR = /usr
 ARDUINO_PORT = /dev/ttyUSB0
 BOARD_TAG = mega328
--- laveuse.ino
+++ laveuse.ino
@@ -1,9 +1,5 @@
 #include <Bounce2.h>
-#include <SPI.h>
 #include <setjmp.h>
-
-#include <PlayingWithFusion_MAX31865.h>        // core library
-#include <PlayingWithFusion_MAX31865_STRUCT.h> // struct library
 
 /* interface utilisateur */
 #define PIN_ARRET 2         // arrêt d'urgence (fil jaune) -- interruption
@@ -39,16 +35,6 @@
 #define bac(X) digitalWrite(PIN_BAC, !X)
 #define recup(X) digitalWrite(PIN_RECUP, !X)
 #define pompe(X) digitalWrite(PIN_POMPE, !X)
-#define plongeur(X) digitalWrite(PIN_PLONGEUR, !X)
-#define plongeurRead !digitalRead(PIN_PLONGEUR)
-
-//Sensor addresses:
-const byte CONFIG_REG_W = 0x80; // Config register write addr
-const byte CONFIG_VALUE = 0xC3; // Config register value (Vbias+Auto+FaultClear+50Hz, see pg 12 of datasheet)
-const byte ADC_WORD_MSB = 0x01; // Addr of first byte of data (start reading at RTD MSB)
-const byte ADC_FAULT_REG = 0x07; // Addr of the fault reg
-
-PWFusion_MAX31865_RTD sonde(PIN_SONDE);
 
 // Boutons pour lancer les cycles de nettoyage et désinfection
 Bounce btnNettoyage = Bounce();
@@ -59,63 +45,6 @@ jmp_buf env; // pour faire le saut en cas d'arrêt d'urgence
 
 /* l'état dans lequel se trouve la laveuse */
 static enum {ARRET, NETTOYAGE, DESINFECTION} etat;
-double temp = 0;
-
-/*
- * mesurer la température de l'eau et retourner le résultat en degrés
- */
-void temperature() {
-    static struct var_max31865 sonde_reg;
-    temp = 0;
-
-    sonde_reg.RTD_type = 2;                        // un-comment for PT1000 RTD
-
-    sonde.MAX31865_full_read(&sonde_reg);          // Update MAX31855 readings
-
-    if(0 == sonde_reg.status) {
-        // calculate RTD temperature (simple calc, +/- 2 deg C from -100C to 100C)
-        // more accurate curve can be used outside that range
-        temp = ((double)sonde_reg.rtd_res_raw / 32) - 256;
-        Serial.print(temp);        // print RTD resistance
-        Serial.println(" deg C"); // print RTD temperature heading
-    }
-    else {
-        Serial.print("RTD Fault, register: ");
-        Serial.println(sonde_reg.status);
-        if(0x80 & sonde_reg.status) {
-            Serial.println("RTD High Threshold Met");  // RTD high threshold fault
-        }
-        else if(0x40 & sonde_reg.status) {
-            Serial.println("RTD Low Threshold Met");   // RTD low threshold fault
-        }
-        else if(0x20 & sonde_reg.status) {
-            Serial.println("REFin- > 0.85 x Vbias");   // REFin- > 0.85 x Vbias
-        }
-        else if(0x10 & sonde_reg.status) {
-            Serial.println("FORCE- open");             // REFin- < 0.85 x Vbias, FORCE- open
-        }
-        else if(0x08 & sonde_reg.status) {
-            Serial.println("FORCE- open");             // RTDin- < 0.85 x Vbias, FORCE- open
-        }
-        else if(0x04 & sonde_reg.status) {
-            Serial.println("Over/Under voltage fault");  // overvoltage/undervoltage fault
-        }
-        else {
-            Serial.println("Unknown fault, check connection"); // print RTD temperature heading
-        }
-    }  // end of fault handling
-}
-
-void chauffer() {
-    temperature();
-    if (!plongeurRead && temp < TEMP_MIN) {
-        Serial.println("Activer la chauffe");
-        plongeur(1);
-    } else if (plongeurRead && temp > TEMP_MAX) {
-        Serial.println("Couper la chauffe");
-        plongeur(0);
-    }
-}
 
 /*
  * attendre X secondes, vérifier la chauffe toutes les secondes et lancer la
@@ -129,17 +58,12 @@ void attendre(int secondes) {
             etat = ARRET;
             Serial.println("Arret d'urgence");
             pompe(0);
-            plongeur(0);
             reseau(0);
             bac(0);
             delay(DUREE_ECOULEMENT*1000);
             recup(0);
             egout(0);
             longjmp(env, 1);
-        }
-        if (millis() % 1000 == 0 && etat == NETTOYAGE) {
-            chauffer();
-            delay(1);
         }
     }
 }
@@ -177,10 +101,6 @@ void nettoyage() {
     etat = NETTOYAGE;
     // pré-rincage
     rincage(10);
-    // attendre que la température soit bonne
-    while (temp < TEMP_MAX-1) {
-        attendre(10);
-    }
     recup(1);
     bac(1);
     attendre(3);
@@ -243,20 +163,13 @@ void setup() {
     pinMode(PIN_BAC, OUTPUT);
     pinMode(PIN_RECUP, OUTPUT);
     pinMode(PIN_POMPE, OUTPUT);
-    pinMode(PIN_PLONGEUR, OUTPUT);
-    pinMode(PIN_SONDE, OUTPUT);
     // état initial de la machine
     etat = ARRET;
     pompe(0);
-    plongeur(0);
     reseau(0);
     bac(0);
     egout(0);
     recup(0);
-    // initialiser la lecture de température
-    SPI.begin();
-    SPI.setDataMode(SPI_MODE3);
-    sonde.MAX31865_config();
     delay(100);
 }
 
